e8d96f6286522c4274fd280205707122e347eca2
SirDuckly <77992530+SirDuckly@users.noreply.github.com>
2022-03-03 22:09:45 +0000

    Update

--- server/main.py
+++ server/main.py
@@ -1,10 +1,23 @@
 import sys
-def func(args):
+def getArgs(args):
     return 2*args
 
+def setPort():
+    return result
+
 if __name__ == "__main__":
-    arguments = func(sys.argv)
+    arguments = getArgs(sys.argv)
     args = arguments[1] + ' ' + arguments[2]
     print (args)
+    print (arguments)
     print ('Starting  server....')
     port = arguments[1]
+
+"""
+functions:
+Set port 
+Start listening session 
+On connection create session
+On session creation ask if want to join/ create room
+
+"""
